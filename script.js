30f561d54ed59d4d3b7599219aee150a56229daa
asifaa3546-creator <asifaa3546@gmail.com>
2026-08-03 14:19:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   alert("👋 Welcome to ASIF TOPUP!");
-});
+});function orderNow(){
+  const uid=document.getElementById("uid").value;
+  const pack=document.getElementById("package").value;
+
+  const msg=`Hello ASIF TOPUP!
+UID: ${uid}
+Package: ${pack}`;
+
+  window.open(
+    "https://wa.me/8801341181330?text="+encodeURIComponent(msg),
+    "_blank"
+  );
+}
