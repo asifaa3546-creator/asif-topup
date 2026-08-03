e2faa446e7fe645431cc27146511cee25f9d4822
asifaa3546-creator <asifaa3546@gmail.com>
2026-08-03 19:30:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ window.open(url, "_blank");
 </select>
 
 <button onclick="sendOrder()">Order Now</button>
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current >= slides.length){
+current = 0;
+}
+
+slides[current].classList.add("active");
+
+},3000);
