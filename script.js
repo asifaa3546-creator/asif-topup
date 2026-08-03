b9943c6e79186a830f5497dffb3161d2f709db53
asifaa3546-creator <asifaa3546@gmail.com>
2026-08-03 19:13:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,24 @@ Package: ${pack}`;
     "https://wa.me/8801341181330?text="+encodeURIComponent(msg),
     "_blank"
   );
+}function sendOrder() {
+
+let uid = document.getElementById("uid").value;
+let player = document.getElementById("player").value;
+let pack = document.getElementById("package").value;
+
+let message =
+`🎮 ASIF TOPUP BD
+
+UID: ${uid}
+
+Player: ${player}
+
+Package: ${pack}`;
+
+let url =
+`https://wa.me/8801341181330?text=${encodeURIComponent(message)}`;
+
+window.open(url, "_blank");
+
 }
